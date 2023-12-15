f47bf6a9effa844c249ae36e63dedb9354c06f0c
YamagataMiho <149132492+YamagataMiho@users.noreply.github.com>
2023-12-15 22:00:44 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,252 +1,114 @@
-// 
-$(window).on('load', function () {
-  const videos = $('.js_inlineVideo');
-  if (videos.length) {
-    playVideos(videos);
-    $(window).on('scroll', function () {
-      playVideos(videos);
-    });
-  }
-  function playVideos(videos) {
-    const startPosition = $(window).scrollTop() + $(window).height();
-    videos.each(function (index) {
-      if (startPosition > $(this).offset().top - 200) {
-        $(this).get(0).play();
-      }
-    });
-  }
-});
-// 
+const question = [
+    ["問題 1", "2030年万博の開催地に決まった都市は？", "選択肢1 : ローマ", "選択肢2 : 釜山", "選択肢3 : リヤド", 3],
+    ["問題 2", "国軍と準軍事組織（RSF）の対立が激化している国は？", "選択肢1 : イスラエル", "選択肢2 : スーダン", "選択肢3 : インド", 2],
+    ["問題 3", "11月2日に包括的核実験禁止条約（CTBT)の批准を撤回した国は？", "選択肢1 : アメリカ", "選択肢2 : 中国", "選択肢3 : ロシア", 3],
+    ["問題 4", "BRICSには新たに何か国が加わることになったか？", "選択肢1 : 4ヵ国", "選択肢2 : 5ヵ国", "選択肢3 : 6ヵ国", 3],
+    ["問題 5", "インボイス制度の正式名称は？", "選択肢1 : 適格請求書等保存方式", "選択肢2 : 公正証書保管方式", "選択肢3 : 適格受領書等保管方法", 1]
+];
+
+let countDownTimer = 40;// 制限時間
+let successFlag = false;// 最後まで解答したか
+let successCount = 0;// 正答数
+let questionCount = 0;// 問題数
 
 
+// 最初は問題を解くボタンだけ表示
+document.getElementById("ansArea").style.visibility = "hidden";
 
-
-
-
-// sectionタイトル背景バー
-function fadeAnime(){
-  
-    $('.fadeLeftTrigger').each(function(){ //fadeLeftTriggerというクラス名が
-      var elemPos = $(this).offset().top-50;//要素より、50px上の
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight){
-      $(this).addClass('fadeLeft');// 画面内に入ったらfadeLeftというクラス名を追記
-      }else{
-      $(this).removeClass('fadeLeft');// 画面外に出たらfadeLeftというクラス名を外す
-      }
-      });
-      
-    // Work boxふわっと
-    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-      var elemPos = $(this).offset().top-50;//要素より、50px上の
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight){
-      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-      }else{
-      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
-      }
-      });
-      // Work boxふわっと----ここまで----
-  }
-  // sectionタイトル背景バー ----ここまで----
-
-// sectionタイトル 文字パタパタ
-  var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-  };
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-  };
-
-  // sectionタイトル 文字パタパタ ----ここまで----
-
-// .Work modalの開閉
-  $(function(){
-    // 変数に要素を入れる
-    var open = $('.modal-open'),
-      close = $('.modal-close');
-      modalContainer = $('.modal-container');
-  
-    //開くボタンをクリックしたらモーダルを表示する
-    open.on('click',function(){	
-      var btnNo = $(this).attr("data-number");
-      var targetId = "#modal" + btnNo;
-      $(targetId).addClass("active");
-      $('body').css('overflow-y', 'hidden');
-      console.log("ccc");
-      return false;
-    });
-  
-    //閉じるボタンをクリックしたらモーダルを閉じる
-    close.on('click',function(){
-      modalContainer.removeClass("active");
-      $('body').css('overflow-y', 'auto');
-      console.log("ddd");
-      return false;
-    });
-  
-    //モーダルの外側をクリックしたらモーダルを閉じる
-    $(document).on('click',function(e) {
-      console.log("aaaa");
-      if(!$(e.target).closest('.active').length){
-        console.log("bbb");
-        modalContainer.removeClass("active");
-        $('body').css('overflow-y', 'auto');
-      }
-    });
-  });
-// .Work modalの開閉 ----ここここまで----
-
-
-
-// ----top タイトルのアニメーション----
-
-// glowAnimeにglowというクラス名を付ける定義
-function GlowAnimeControl() {
-  $('.glowAnime').each(function () {
-    var elemPos = $(this).offset().top - 50;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass("glow");
-
-    } else {
-      $(this).removeClass("glow");
-    }
-  });
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-// 画面が読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-  //spanタグを追加する
-    var element = $(".glowAnime");
-  element.each(function () {
-    var text = $(this).text();
-    var textbox = "";
-    text.split('').forEach(function (t, i) {
-      if (t !== " ") {
-        if (i < 10) {
-          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+const countTimer = () => {
+    if (successFlag == false) {
+        document.getElementById("countDown").innerHTML = `残り${countDownTimer}秒です`
+        if (!countDownTimer == 0) {
+            setTimeout(() => {
+                countDownTimer = countDownTimer - 1;
+                countTimer();
+            }, 1000);
         } else {
-        var n = i / 10;
-          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+            setTimeout(() => {
+                alert("時間切れ");
+                // カウントをリセットする
+                countDownTimer = 40;
+                questionCount = 0;
+                successCount = 0;
+
+                // 問題を非表示にし、ボタンを表示する
+                document.getElementById("ansStartButton").style.visibility = "visible";
+                document.getElementById("ansArea").style.visibility = "hidden";
+                // ボタンの表示を変える
+                document.getElementById("ansStartButton").innerHTML = "もう一度挑戦する";
+            })
         }
-
-      } else {
-        textbox += t;
-      }
-    });
-    $(this).html(textbox);
-  });
-
-  GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-//
-
-// ----top タイトルのアニメーション ----ここまで----
-
-
-// -----ハンバーガーメニュー-----
- 
- $(function() {
-   $('.Toggle').click(function() {
-    $(this).toggleClass('active');
-   $('.menu').toggleClass('open');
-  });
- });
-// -----ハンバーガーメニュー-----ここまで------
-
-// Top 漢字のアニメーション
-function BgFadeAnime(){
-
-$('.bgLRextendTrigger').each(function(){ //bgLRextendTriggerというクラス名が
-  var elemPos = $(this).offset().top-50;//要素より、50px上の
-  var scroll = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  if (scroll >= elemPos - windowHeight){
-    $(this).addClass('bgLRextend');// 画面内に入ったらbgLRextendというクラス名を追記
-  }else{
-    $(this).removeClass('bgLRextend');// 画面外に出たらbgLRextendというクラス名を外す
-  }
-}); 
-
- // 文字列を囲う子要素
-$('.bgappearTrigger').each(function(){ //bgappearTriggerというクラス名が
-  var elemPos = $(this).offset().top-50;//要素より、50px上の
-  var scroll = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  if (scroll >= elemPos - windowHeight){
-    $(this).addClass('bgappear');// 画面内に入ったらbgappearというクラス名を追記
-  }else{
-    $(this).removeClass('bgappear');// 画面外に出たらbgappearというクラス名を外す
-  }
-});   
+    }
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function (){
-  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面をスクロールをしたら動かしたい場合の記述
 
-// 画面が読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function(){
-  BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-// Top 漢字のアニメーション ----ここまで----
+const viewQuestion = () => {
+    // 問題文表示
+    document.getElementById("question").innerHTML = question[questionCount][0];
+    // 選択肢表示
+    document.getElementById("ans1").innerHTML = question[questionCount][1];
+    document.getElementById("ans2").innerHTML = question[questionCount][2];
+    document.getElementById("ans3").innerHTML = question[questionCount][3];
+    document.getElementById("ans4").innerHTML = question[questionCount][4];
+}
+
+
+const ansButton = (e) => {
+    if (e == question[questionCount][5]) {
+        alert("正解");
+        successCount = successCount + 1;
+        // successCount++;
+    } else {
+        alert("不正解");
+    }
+    // 問題数のカウントを増やす
+    questionCount = questionCount + 1;
+    // questionCount++;
+
+    if (questionCount == question.length) {
+        document.getElementById("answer").innerHTML = `${successCount}問正解です`;
+        document.getElementById("countDown").style.visibility = "hidden";
+        successFlag = true;
+    } else {
+        viewQuestion();
+    }
+}
+
+// 
+// const ansButton = (e) => {
+//     if (e == question[questionCount][5]){
+//         document.write("<h1>〇</h1>");
+//         successCount = successCount + 1;
+//     } else {
+//         document.write("<h1>✖</h1>");
+//     }
+//         // 問題数のカウントを増やす
+//         questionCount = questionCount + 1;
+//         // questionCount++;
+    
+//         if (questionCount == question.length) {
+//             document.getElementById("answer").innerHTML = `${successCount}問正解です`;
+//             document.getElementById("countDown").style.visibility = "hidden";
+//             successFlag = true;
+//         } else {
+//             viewQuestion();
+//         }
+    
+// }
+// 
+
+
+
+
+
+
+const ansStart = () => {
+    // 問題を解くボタンを消す
+    document.getElementById("ansStartButton").style.visibility = "hidden";
+    // 問題文と選択肢を表示
+    document.getElementById("ansArea").style.visibility = "visible";
+    countTimer();
+    viewQuestion();
+
+}
+
